de591cf38e52c00ae730c0a53969d9adbe31e192
silver-kg <kge8606@kau.kr>
2025-03-27 20:19:07 +0900

    250327 frontend

---
+++ src/pages/ProductPage/New.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const New = () => {
+    return(
+        <div>
+            <h1>야 너두 사장!!</h1>
+            <p>멋쟁이 나처럼 13기</p>
+        </div>
+    );
+};
+
+export default New;
